6901a9b5f4d3cb9d7d8707bf079a8f476d28c0fe
luzk-mb <mejiakarine20@gmail.com>
2024-10-14 20:16:59 -0500

    script

---
+++ script.js
@@ -0,0 +1,14 @@
+function validarFormulario(){
+    let nombre=document.getElementById("nombre").value;
+    let email=document.getElementById("email").value;
+    let imagen=document.getElementById("imagen").value;
+    if(nombre=="" || email=="" || imagen==""){
+        alert("todos lo campos son obligatorios...");
+        return false;
+    }
+    return true;
+}
+function mostrarImagen(event){
+    let imagen=document.getElementById('ver-imagen');
+    imagen.src=URL.createObjectURL(event.target.files[0]);
+}
